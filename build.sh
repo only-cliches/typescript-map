1cfabf47ffba837d52298a10ec5a10fa23fc9543
Scott Lott <scott@clicksimply.com>
2016-12-08 22:59:25 -0800

    changed build system to work better with NPM

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ./node_modules/.bin/tsc
+cp dist/index.js index.js
+./node_modules/.bin/webpack --output-library-target 'commonjs2' index.js index.js
 cd dist
 ./../node_modules/.bin/webpack -p --output-library-target 'umd' index.js tsmap.min.js
 rm index.js
